586dcb0426c079b4def681ec9803c42c82f01452
Ely De La Cruz <elycruz@elycruz.com>
2018-05-06 22:39:46 -0400

    chp7 - HelloCube.jsx.

--- src/components/chp7/HelloCube.jsx
+++ src/components/chp7/HelloCube.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {error} from '../../utils/utils';
 import {
     getWebGlContext, initProgram, getAttribLoc as attribLoc, getUniformLoc as uniformLoc,
@@ -55,25 +55,36 @@ export default class ZFighting extends GenericCanvasExperimentView {
         }
 
         function initVertexBuffers (glContext) {
-            const verticeAndSizes = new Float32Array([
-                    // X, Y, Z, R, G, B
-                    // Triangles in the center
-                     0.0,  2.5, -5.0, 0.0, 1.0, 0.0,  // Blue triangle in front
-                    -2.5, -2.5, -5.0, 0.0, 1.0, 0.0,
-                     2.5, -2.5, -5.0, 1.0, 0.0, 0.0,
-
-                     0.0,  3.0, -5.0, 1.0, 0.0, 0.0,  // Blue triangle in front
-                    -3.0, -3.0, -5.0, 1.0, 1.0, 0.0,
-                     3.0, -3.0, -5.0, 1.0, 1.0, 0.0
+            const
+                verticesAndColors = new Float32Array([
+                    // Vertice and colors
+                    1.0,  1.0,  1.0,     1.0,  1.0,  1.0,  // v0 White
+                    -1.0,  1.0,  1.0,     1.0,  0.0,  1.0,  // v1 Magenta
+                    -1.0, -1.0,  1.0,     1.0,  0.0,  0.0,  // v2 Red
+                    1.0, -1.0,  1.0,     1.0,  1.0,  0.0,  // v3 Yellow
+                    1.0, -1.0, -1.0,     0.0,  1.0,  0.0,  // v4 Green
+                    1.0,  1.0, -1.0,     0.0,  1.0,  1.0,  // v5 Cyan
+                    -1.0,  1.0, -1.0,     0.0,  0.0,  1.0,  // v6 Blue
+                    -1.0, -1.0, -1.0,     0.0,  0.0,  0.0   // v7 Black
                 ]),
-                FSIZE = verticeAndSizes.BYTES_PER_ELEMENT,
-                vertexBuffer = glContext.createBuffer(),
+                // Face indice triangles
+                indices = new Uint8Array([
+                    0, 1, 2,  0, 2, 3, // front
+                    0, 3, 4,  0, 4, 5, // right
+                    0, 5, 6,  0, 6, 1, // top
+                    1, 6, 7,  1, 7, 2, // left
+                    7, 4, 3,  7, 3, 2, // bottom
+                    4, 7, 6,  4, 6, 5  // back
+                ]),
+                FSIZE = verticesAndColors.BYTES_PER_ELEMENT,
+                vertexAndColorBuffer = glContext.createBuffer(),
+                indexBuffer = glContext.createBuffer(),
                 a_Position = attribLoc(gl, 'a_Position'),
                 a_Color = attribLoc(gl, 'a_Color')
             ;
 
-            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-            gl.bufferData(gl.ARRAY_BUFFER, verticeAndSizes, gl.STATIC_DRAW);
+            gl.bindBuffer(gl.ARRAY_BUFFER, vertexAndColorBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, verticesAndColors, gl.STATIC_DRAW);
 
             gl.vertexAttribPointer(a_Position, 3, gl.FLOAT,  false, FSIZE * 6, 0);
             gl.enableVertexAttribArray(a_Position);
@@ -81,7 +92,11 @@ export default class ZFighting extends GenericCanvasExperimentView {
             gl.vertexAttribPointer(a_Color, 3, gl.FLOAT,  false, FSIZE * 6, FSIZE * 3);
             gl.enableVertexAttribArray(a_Color);
 
-            return !vertexBuffer ? -1 : verticeAndSizes.length / 6; // num sides in shape
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+            return !vertexAndColorBuffer || !indexBuffer ? -1 :
+                indices.length; // num sides in shape
         }
 
         const numCreatedVertices = initVertexBuffers(gl);
@@ -90,17 +105,17 @@ export default class ZFighting extends GenericCanvasExperimentView {
             error('Error while creating vertices buffer.');
         }
                                 //  x   y   z
-        const eye = vec3.fromValues(0,  0,  13),  // Get converted to floating point
-            currFocal = vec3.fromValues(0,  0, -100),
-            upFocal =   vec3.fromValues(0,  1,  0),
+        const eye =       vec3.fromValues(3,  3,  7),  // Get converted to floating point
+            currFocal =   vec3.fromValues(0,  0,  0),
+            upFocal =     vec3.fromValues(0,  1,  0),
             u_MvpMatrix = uniformLoc(gl, 'u_MvpMatrix'),
-            viewMatrix = mat4.create(),
-            projMatrix = mat4.create(),
+            viewMatrix =  mat4.create(),
+            projMatrix =  mat4.create(),
             modelMatrix = mat4.create(),
-            mvpMatrix = mat4.create();
+            mvpMatrix =   mat4.create();
 
         mat4.lookAt(viewMatrix, eye, currFocal, upFocal);
-        mat4.translate(modelMatrix, modelMatrix, vec3.fromValues(0.75, 0.0, 0.0));
+        // mat4.translate(modelMatrix, modelMatrix, vec3.fromValues(0.75, 0.0, 0.0));
         mat4.perspective(projMatrix, toRadians(30), canvasElm.offsetWidth / canvasElm.offsetHeight, 1, 100);
         mat4.multiply(mvpMatrix, projMatrix, viewMatrix);
         mat4.multiply(mvpMatrix, mvpMatrix, modelMatrix);
@@ -112,18 +127,11 @@ export default class ZFighting extends GenericCanvasExperimentView {
         gl.enable(gl.POLYGON_OFFSET_FILL);
         gl.polygonOffset(1.0, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        gl.drawArrays(gl.TRIANGLES, 0, numCreatedVertices);
-
-        mat4.translate(modelMatrix, modelMatrix, vec3.fromValues(-1.50, 0.0, 0.0));
-        mat4.multiply(mvpMatrix, projMatrix, viewMatrix);
-        mat4.multiply(mvpMatrix, mvpMatrix, modelMatrix);
-        gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix);
-        gl.drawArrays(gl.TRIANGLES, 0, numCreatedVertices);
+        gl.drawElements(gl.TRIANGLES, numCreatedVertices, gl.UNSIGNED_BYTE, 0);
     }
 
     render () {
         const {props} = this;
-
         return (
             <div>
                 <header>
